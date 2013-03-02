c4fc09487ce80f87f4bc6b497bb36aad3dbf92c7
Adam Patterson <apatterson@discoverysoftware.com>
2013-03-01 17:49:13 -0800

    Fixes the fixation efect so that when the fixation div is fixed an inner div now expands to page width and is centred. The nav is then given position absolute and positioned to the right of the centred div. In effect the fixation div takes over for the header.

--- _parts/page-header.php
+++ _parts/page-header.php
@@ -2,10 +2,12 @@
 	<div>
 		<h1><a href="/">TauntaunWonton</a><em>Comfort food for your browser</em></h1>
 		<div class="fixation">
-			<nav>
-				<a href="/">main</a>
-				<a href="/resume">resume</a>
-			</nav>
+			<div>
+				<nav>
+					<a href="/">main</a>
+					<a href="/resume">resume</a>
+				</nav>
+			</div>
 		</div>
 	</div>
 </header>
