187f845ea723b2288f32c83ea93b98cd95019b69
Adam Patterson <apatterson@discoverysoftware.com>
2013-04-13 17:41:47 -0700

    JS type fix for IE

--- _parts/site-header.php
+++ _parts/site-header.php
@@ -7,7 +7,7 @@
 <html lang=en>
 	<head>
 		<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-		<meta http-equiv="content-script-type" content="application/javascript">
+		<meta http-equiv="content-script-type" content="text/javascript">
 	
 		<title>TauntaunWonton Consulting</title>
 	
@@ -40,11 +40,11 @@
 		<link href="/resources/styles/<?= $page; ?>.css" rel="stylesheet" type="text/css">
 
 		<!-- Pull in jQuery -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="application/javascript"></script>
-		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js" type="application/javascript"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js" type="text/javascript"></script>
 	
 		<!-- Tag for browser/feature specificity -->
-		<script type="application/javascript">
+		<script type="text/javascript">
 		function css_browser_selector(u){var ua = u.toLowerCase(),is=function(t){return ua.indexOf(t)>-1;},g='gecko',w='webkit',s='safari',o='opera',h=document.documentElement,b=[(!(/opera|webtv/i.test(ua))&&/msie\s(\d)/.test(ua))?('ie ie'+RegExp.$1):is('firefox/2')?g+' ff2':is('firefox/3.5')?g+' ff3 ff3_5':is('firefox/3')?g+' ff3':is('gecko/')?g:is('opera')?o+(/version\/(\d+)/.test(ua)?' '+o+RegExp.$1:(/opera(\s|\/)(\d+)/.test(ua)?' '+o+RegExp.$2:'')):is('konqueror')?'konqueror':is('chrome')?w+' chrome':is('iron')?w+' iron':is('applewebkit/')?w+' '+s+(/version\/(\d+)/.test(ua)?' '+s+RegExp.$1:''):is('mozilla/')?g:'',is('j2me')?'mobile':is('iphone')?'iphone':is('ipod')?'ipod':is('mac')?'mac':is('darwin')?'mac':is('webtv')?'webtv':is('win')?'win':is('freebsd')?'freebsd':(is('x11')||is('linux'))?'linux':'','js']; c = b.join(' '); h.className += ' '+c; return c;}; css_browser_selector(navigator.userAgent);
 		</script>
 	</head>
